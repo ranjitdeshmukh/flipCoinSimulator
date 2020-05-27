e53340d1a7e8ed11c2fd04909e8878898e25c34a
Shubham <s.shrinath@splendornet.com>
2020-05-27 07:24:43 +0530

    Added :  Flipping a Coin Functionality

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,12 @@
+#!/bin/bash -x
 echo "Welcome Flip Coin Simulation Problem"
+
+
+ choice=$((RANDOM % 2 + 1))
+
+ if [ $choice -eq "1" ]
+ then
+    echo "  heads win"
+ else
+    echo "  tails wins"
+ fi 
